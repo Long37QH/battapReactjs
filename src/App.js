5e3eb1f90572708f6d322c2194458bead8fbc244
Dinh Viet Long <long37k1999@gmail.com>
2023-03-18 20:56:26 +0700

    sua file appjs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          bai thuc hanh nodej dau tien
         </p>
         <a
           className="App-link"
